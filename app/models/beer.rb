a3334a036dcd9491f590eff22e637b7f6cd5e2d9
Your Name <my.address@gmail.com>
2016-02-02 16:58:51 +0200

    second week 0.2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,5 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,10 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
+
+  def to_s
+    name = beer.name
+    "#{name} #{score}"
+  end
+
+
 end
